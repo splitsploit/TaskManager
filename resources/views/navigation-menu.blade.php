d999e42795af05279ad45961d1e20bb608d5699b
unknown <bayuxbca@gmail.com>
2023-01-06 13:32:30 +0700

    update view and routing

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('tasks.index') }}" :active="request()->routeIs('tasks')">
+                        {{ __('Task List') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
