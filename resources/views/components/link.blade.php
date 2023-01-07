8631fffa543d4e9ef34a77f656083481046dcb94
unknown <bayuxbca@gmail.com>
2023-01-07 10:31:41 +0700

    finalizing styling apps

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,7 +1,7 @@
-{{-- <button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center ...']) }}>
+{{-- <a {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center ...']) }}>
     {{ $slot }}
-</button> --}}
+</a> --}}
 
-<a {{ $attributes->merge(['class' => 'inline-flex items-center ...']) }}>
+<a {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-purple-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition']) }}>
     {{ $slot }}
 </a>
